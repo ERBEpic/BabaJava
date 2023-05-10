1c82d2ac2a69f7c686029297830d3d4994f99baa
Joespeh <braunsteineli@gmail.com>
2023-05-10 11:28:00 -0400

    levelCreator+loading levels is working.

--- src/levelCreator.java
+++ src/levelCreator.java
@@ -3,7 +3,7 @@ import java.lang.*;
 public class levelCreator {
     static int[][][][] level = new int[20][20][0][5];
     public static void add(int x, int y, int id, int rotation){
-        int z = findValidAndExpand(x,y);
+        int z = findExpand(x,y);
         level[x][y][z][0]=id;
         level[x][y][z][1]=rotation;
         level[x][y][z][2]=0;
@@ -11,7 +11,7 @@ public class levelCreator {
         level[x][y][z][4]=0;
     }
     public static void add(int x, int y, int id){
-        int z = findValidAndExpand(x,y);
+        int z = findExpand(x,y);
         level[x][y][z][0]=id;
         level[x][y][z][1]=0;
         level[x][y][z][2]=0;
@@ -32,7 +32,7 @@ public class levelCreator {
                 catch (Exception a){
                     System.out.println("bye");
                     int[][][][] tempArray = new int[20][20][z+1][5];
-                    System.arraycopy(level,0, tempArray,0,);
+                    System.arraycopy(level,0, tempArray,0,z);
                     level = tempArray;
                     return z;
                 }
@@ -40,10 +40,28 @@ public class levelCreator {
             System.out.println("hai");
         }
     }
+    public static int findExpand(int x, int y){
+        int currentDepth = level[x][y].length;
+
+        int[][] newArray = new int[currentDepth + 1][5];
+
+        for (int z = 0; z < currentDepth; z++) {
+            for (int i = 0; i < 5; i++) {
+                newArray[z][i] = level[x][y][z][i];
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            newArray[currentDepth][i] = 0;
+        }
+
+        level[x][y] = newArray;
+        return currentDepth;
+    }
     public static void main(String[] args) throws IOException {
         add(6,7,3);
         add(2,4,6);
-        add(3,4,6);
+        add(5,4,6);
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("level1.data")));
         oos.writeObject(level);
         oos.flush();
