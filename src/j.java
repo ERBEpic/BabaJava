20cfd626b1b6c5b2c0f70b72891edfc72fe55436
Joespeh <braunsteineli@gmail.com>
2023-05-18 14:34:18 -0400

    Changes for java 17. I want to make j into the new BabaFrame, as it works properly, and is a lot simpler. Ive taken some base code from GPT and am modifying it a good bit to be my own. I dont think this should be disallowed, as its not just copy paste.

--- src/j.java
+++ src/j.java
@@ -1,56 +1,80 @@
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class j extends JFrame {
+
     private int tileSize;
     private int numTilesX;
     private int numTilesY;
-    private Image[][][] tileMap;
+    private LinkedList<ImageLayer>[] tileMap;
 
     public j(int tileSize, int numTilesX, int numTilesY) {
-        this.setUndecorated(true);
-        this.setBackground(new Color(0, 0, 0, 0)); // frame is transparent
-        this.getRootPane().setOpaque(false); // frame content (rootpane) is transparent...
-        this.setOpacity(0.0f); // or maybe this achieves the same thing
+        //this.setUndecorated(true);
+        //this.setBackground(new Color(182, 42, 42)); // frame is transparent
+        this.getRootPane().setOpaque(true); // frame content (rootpane) is transparent...
+        //this.setOpacity(0.0f); // or maybe this achieves the same thing
         this.tileSize = tileSize;
         this.numTilesX = numTilesX;
         this.numTilesY = numTilesY;
-        this.tileMap = new Image[numTilesX][numTilesY][2];
-
+        this.tileMap = new LinkedList[numTilesX * numTilesY];
+        for (int i = 0; i < numTilesX * numTilesY; i++) {
+            tileMap[i] = new LinkedList<ImageLayer>();
+        }
         setSize(numTilesX * tileSize, numTilesY * tileSize);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(Color.BLACK);
         setVisible(true);
+
     }
 
-    public void setImage(int x, int y, int z, Image image) {
-        tileMap[x][y][z] = image;
+    public void addImage(int x, int y, Image image, int layer) {
+        tileMap[y * numTilesX + x].add(new ImageLayer(image, layer));
         repaint();
     }
 
-    public void paint(Graphics g) {
-        for (int x = 0; x < numTilesX; x++) {
-            for (int y = 0; y < numTilesY; y++) {
-                Image bottomImage = tileMap[x][y][0];
-                Image topImage = tileMap[x][y][1];
-                if (bottomImage != null) {
-                    g.drawImage(bottomImage, x * tileSize, y * tileSize, tileSize, tileSize, null);
-                }
-                if (topImage != null) {
-                    g.drawImage(topImage, x * tileSize, y * tileSize, tileSize, tileSize, null);
-                }
+    public void removeImage(int x, int y, int layer) {
+        LinkedList<ImageLayer> layers = tileMap[y * numTilesX + x];
+        for (int i = 0; i < layers.size(); i++) {
+            if (layers.get(i).layer == layer) {
+                layers.remove(i);
+                repaint();
+                return;
             }
         }
     }
 
+    public void paint(Graphics g) {
+        super.paint(g);
+        for (int i = 0; i < numTilesX * numTilesY; i++) {
+            int x = i % numTilesX;
+            int y = i / numTilesX;
+            LinkedList<ImageLayer> layers = tileMap[i];
+            for (ImageLayer layer : layers) {
+                g.drawImage(layer.image, x * tileSize, y * tileSize, tileSize, tileSize, null);
+            }
+        }
+    }
+
+    private class ImageLayer {
+        public Image image;
+        public int layer;
+        public ImageLayer(Image image, int layer) {
+            this.image = image;
+            this.layer = layer;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        j grid = new j(24, 5, 5);
-        grid.setImage(2, 2, 0, ImageIO.read(new File("Sprites/wall_9_2.png")));
-        grid.setImage(1, 3, 0, ImageIO.read(new File("Sprites/wall_9_2.png")));
-        grid.setImage(1, 2, 0, ImageIO.read(new File("Sprites/wall_9_2.png")));
-        grid.setImage(1, 2, 1, ImageIO.read(new File("Sprites/algae_0_1.png")));
+        j grid = new j(24, 21, 21);
+        grid.addImage(2, 2, ImageIO.read(new File("Sprites/wall_9_2.png")), 0);
+        grid.addImage(1, 3, ImageIO.read(new File("Sprites/wall_9_2.png")), 0);
+        grid.addImage(1, 2, ImageIO.read(new File("Sprites/wall_9_2.png")), 0);
+        grid.addImage(1, 2, ImageIO.read(new File("Sprites/algae_0_1.png")), 1);
+        grid.addImage(1, 2, ImageIO.read(new File("Sprites/algae_0_1.png")), 4);
 
     }
 }
