4e3f1ba643a894ab2ce27f3b2d8fc3aa36629834
Joespeh <braunsteineli@gmail.com>
2023-06-06 12:35:28 -0400

    Made level 2

--- src/levelCreator.java
+++ src/levelCreator.java
@@ -27,84 +27,86 @@ public class levelCreator {
         level[x][y] = newArray;//above this just expands
         return currentDepth;
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {//TODO Change grass to be a connecting type
         for (int i = 0; i < 7; i++) {
-            add(8+i,3,1);
+            add(8+i,3,5);
         }
         for (int i = 0; i < 7; i++) {
-            add(8+i,11,1);
+            add(8+i,11,5);
         }
         for (int i = 0; i < 7; i++) {
-            add(8+i,17,1);
+            add(8+i,17,5);
         }
         for (int i = 0; i < 15; i++) {
-            add(15,3+i,1);
+            add(15,3+i,5);
         }
         for (int i = 0; i < 5; i++) {
-            add(8,3+i,1);
+            add(8,3+i,5);
         }
         for (int i = 0; i < 6; i++) {
-            add(8,11+i,1);
+            add(8,11+i,5);
         }
         for (int i = 0; i < 4; i++) {
-            add(4+i,7,1);
+            add(4+i,7,5);
         }
         for (int i = 0; i < 4; i++) {
-            add(4+i,11,1);
+            add(4+i,11,5);
         }
         for (int i = 0; i < 3; i++) {
-            add(4,8+i,1);
+            add(4,8+i,5);
         }
         for (int i = 0; i < 4; i++) {
-            add(5+i,8,9);
+            add(5+i,8,10);
         }
         for (int i = 0; i < 4; i++) {
-            add(5+i,10,9);
+            add(5+i,10,10);
         }
-        add(5,9,9);
-        add(7,9,9);
-        add(8,9,9);
+        add(5,9,10);
+        add(6,9,27);
+        add(7,9,10);
+        add(8,9,10);
 
 
         add(10,5,12);
         add(13,7,14);
 
-        add(10,9,5);
-
-        add(6,9,25);
 
 
 
-        add(5,13,27);
+
+        add(5,13,21);
         add(5,14,12);
         add(5,15,13);
-        add(10,13,21);
+        add(10,13,25);//right
         add(10,14,12);
         add(10,15,17);
-        add(13,14,7);
+        add(13,14,1);
 
 
 
 
-        add(19,13,10);
-        add(11,1,10);
-        add(5,2,10);
-        add(4,1,10);
+        add(19,13,9);
+        add(11,1,9);
+        add(5,2,9);
+        add(4,1,9);
 
-        add(18,5,10);
-        add(19,5,10);
-
-
-        add(5,19,10);
-        add(5,18,10);
-        add(4,19,10);
-
-        add(1,13,10);
+        add(18,5,9);
+        add(19,5,9);
 
 
+        add(5,19,9);
+        add(5,18,9);
+        add(4,19,9);
+
+        add(1,13,9);
 
 
-        int levelnumber = 1;
+
+
+
+
+
+        int levelnumber = 2;
 
 
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("levels/level"+levelnumber+".data"));
